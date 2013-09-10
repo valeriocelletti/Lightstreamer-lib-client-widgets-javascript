c6cde36723167973db19260846f0844da4ea479c
mone <simone.fabiano@gmail.com>
2013-09-10 16:45:53 +0200

    fix Cell dependency

--- source/Cell.js
+++ source/Cell.js
@@ -13,7 +13,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
-define(["../Environment"], 
+define(["Environment"], 
     function(Environment){
   
   Environment.browserDocumentOrDie();
