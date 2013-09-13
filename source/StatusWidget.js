babfba34cf52fb8c26c33eb3cfebf3774b17a33d
mone <simone.fabiano@gmail.com>
2013-09-13 15:13:48 +0200

    fix reuired module name

--- source/StatusWidget.js
+++ source/StatusWidget.js
@@ -13,7 +13,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
-define(["Environment","IllegalArgumentException","Helpers","./LightstreamerConstants",".Executor","BrowserDetection"], 
+define(["Environment","IllegalArgumentException","Helpers","./LightstreamerConstants","Executor","BrowserDetection"], 
     function(Environment,IllegalArgumentException,Helpers,Constants,Executor,BrowserDetection) {
 
   Environment.browserDocumentOrDie();
