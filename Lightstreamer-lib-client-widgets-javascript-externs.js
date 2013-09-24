4367ac7ca924a62ea46950deaf2e51bd2acc509a
mone <simone.fabiano@gmail.com>
2013-09-24 15:03:09 +0200

    expose own externs

---
+++ Lightstreamer-lib-client-widgets-javascript-externs.js
@@ -0,0 +1,108 @@
+   AbstractWidget.prototype["onItemUpdate"] = AbstractWidget.prototype.onItemUpdate;
+  AbstractWidget.prototype["onClearSnapshot"] = AbstractWidget.prototype.onClearSnapshot;
+  AbstractWidget.prototype["onSubscription"] = AbstractWidget.prototype.onSubscription;
+  AbstractWidget.prototype["onUnsubscription"] = AbstractWidget.prototype.onUnsubscription;
+  AbstractWidget.prototype["onListenStart"] = AbstractWidget.prototype.onListenStart;
+  AbstractWidget.prototype["onListenEnd"] = AbstractWidget.prototype.onListenEnd;
+  AbstractWidget.prototype["removeRow"] = AbstractWidget.prototype.removeRow;
+  AbstractWidget.prototype["updateRow"] = AbstractWidget.prototype.updateRow;
+  AbstractWidget.prototype["clean"] = AbstractWidget.prototype.clean;
+  AbstractWidget.prototype["getValue"] = AbstractWidget.prototype.getValue;
+  AbstractWidget.prototype["setAutoCleanBehavior"] = AbstractWidget.prototype.setAutoCleanBehavior;
+  AbstractWidget.prototype["parseHtml"] = AbstractWidget.prototype.parseHtml;
+  AbstractWidget.prototype["updateRowExecution"] = AbstractWidget.prototype.updateRowExecution;
+  AbstractWidget.prototype["removeRowExecution"] = AbstractWidget.prototype.removeRowExecution;
+  AbstractWidget.prototype["mergeUpdate"] = AbstractWidget.prototype.mergeUpdate;
+  
+  AbstractGrid.prototype["setHtmlInterpretationEnabled"] = AbstractGrid.prototype.setHtmlInterpretationEnabled;
+  AbstractGrid.prototype["isHtmlInterpretationEnabled"] = AbstractGrid.prototype.isHtmlInterpretationEnabled;
+  AbstractGrid.prototype["setNodeTypes"] = AbstractGrid.prototype.setNodeTypes;
+  AbstractGrid.prototype["getNodeTypes"] = AbstractGrid.prototype.getNodeTypes;
+  AbstractGrid.prototype["setAddOnTop"] = AbstractGrid.prototype.setAddOnTop;
+  AbstractGrid.prototype["isAddOnTop"] = AbstractGrid.prototype.isAddOnTop;
+  AbstractGrid.prototype["setSort"] = AbstractGrid.prototype.setSort;
+  AbstractGrid.prototype["getSortField"] = AbstractGrid.prototype.getSortField;
+  AbstractGrid.prototype["isDescendingSort"] = AbstractGrid.prototype.isDescendingSort;
+  AbstractGrid.prototype["isNumericSort"] = AbstractGrid.prototype.isNumericSort;
+  AbstractGrid.prototype["isCommaAsDecimalSeparator"] = AbstractGrid.prototype.isCommaAsDecimalSeparator;
+  AbstractGrid.prototype["extractFieldList"] = AbstractGrid.prototype.extractFieldList;
+  AbstractGrid.prototype["extractCommandSecondLevelFieldList"] = AbstractGrid.prototype.extractCommandSecondLevelFieldList;
+  AbstractGrid.prototype["parseHtml"] = AbstractGrid.prototype.parseHtml;
+  AbstractGrid.prototype["forceSubscriptionInterpretation"] = AbstractGrid.prototype.forceSubscriptionInterpretation;
+  AbstractGrid.prototype["updateRowExecution"] = AbstractGrid.prototype.updateRowExecution;
+  AbstractGrid.prototype["removeRowExecution"] = AbstractGrid.prototype.removeRowExecution;
+  
+  Chart.prototype["parseHtml"] = Chart.prototype.parseHtml;
+  Chart.prototype["configureArea"] = Chart.prototype.configureArea;
+  Chart.prototype["setXAxis"] = Chart.prototype.setXAxis;
+  Chart.prototype["addYAxis"] = Chart.prototype.addYAxis;
+  Chart.prototype["removeYAxis"] = Chart.prototype.removeYAxis;
+  Chart.prototype["positionXAxis"] = Chart.prototype.positionXAxis;
+  Chart.prototype["setXLabels"] = Chart.prototype.setXLabels;
+  Chart.prototype["addListener"] = Chart.prototype.addListener;
+  Chart.prototype["removeListener"] = Chart.prototype.removeListener;
+  Chart.prototype["getListeners"] = Chart.prototype.getListeners;
+  Chart.prototype["clean"] = Chart.prototype.clean;
+  Chart.prototype["onListenStart"] = Chart.prototype.onListenStart;
+  Chart.prototype["updateRowExecution"] = Chart.prototype.updateRowExecution;
+  Chart.prototype["removeRowExecution"] = Chart.prototype.removeRowExecution;
+  
+  
+  
+   //closure compiler exports
+  DynaGrid.prototype["setMaxDynaRows"] = DynaGrid.prototype.setMaxDynaRows;
+  DynaGrid.prototype["getMaxDynaRows"] = DynaGrid.prototype.getMaxDynaRows;
+  DynaGrid.prototype["goToPage"] = DynaGrid.prototype.goToPage;
+  DynaGrid.prototype["getCurrentPages"] = DynaGrid.prototype.getCurrentPages;
+  DynaGrid.prototype["setAutoScroll"] = DynaGrid.prototype.setAutoScroll;
+  DynaGrid.prototype["parseHtml"] = DynaGrid.prototype.parseHtml;
+  DynaGrid.prototype["clean"] = DynaGrid.prototype.clean;
+  DynaGrid.prototype["addListener"] = DynaGrid.prototype.addListener;
+  DynaGrid.prototype["removeListener"] = DynaGrid.prototype.removeListener;
+  DynaGrid.prototype["getListeners"] = DynaGrid.prototype.getListeners;
+  DynaGrid.prototype["updateRowExecution"] = DynaGrid.prototype.updateRowExecution;
+  DynaGrid.prototype["removeRowExecution"] = DynaGrid.prototype.removeRowExecution;
+  
+  
+   SimpleChartListener.prototype["onListenStart"] = SimpleChartListener.prototype.onListenStart;
+  SimpleChartListener.prototype["onYOverflow"] = SimpleChartListener.prototype.onYOverflow;
+  SimpleChartListener.prototype["onXOverflow"] = SimpleChartListener.prototype.onXOverflow;
+  SimpleChartListener.prototype["onNewLine"] = SimpleChartListener.prototype.onNewLine;
+  SimpleChartListener.prototype["onRemovedLine"] = SimpleChartListener.prototype.onRemovedLine;
+  
+  
+   StaticGrid.prototype["addCell"] = StaticGrid.prototype.addCell;
+  StaticGrid.prototype["setRootNode"] = StaticGrid.prototype.setRootNode;
+  StaticGrid.prototype["extractItemList"] = StaticGrid.prototype.extractItemList;
+  StaticGrid.prototype["parseHtml"] = StaticGrid.prototype.parseHtml;
+  StaticGrid.prototype["addListener"] = StaticGrid.prototype.addListener;
+  StaticGrid.prototype["removeListener"] = StaticGrid.prototype.removeListener;
+  StaticGrid.prototype["getListeners"] = StaticGrid.prototype.getListeners;
+  StaticGrid.prototype["updateRowExecution"] = StaticGrid.prototype.updateRowExecution;
+  StaticGrid.prototype["removeRowExecution"] = StaticGrid.prototype.removeRowExecution;
+  
+  StatusBarProgressIndicator.prototype["onStatusChange"] = StatusBarProgressIndicator.prototype.onStatusChange;
+  StatusBarProgressIndicator.prototype["onListenStart"] = StatusBarProgressIndicator.prototype.onListenStart;
+  StatusBarProgressIndicator.prototype["onListenEnd"] = StatusBarProgressIndicator.prototype.onListenEnd;
+
+  
+  StatusImageWidget.prototype["onStatusChange"] = StatusImageWidget.prototype.onStatusChange;
+
+  StatusWidget.prototype["onStatusChange"] = StatusWidget.prototype.onStatusChange;
+  StatusWidget.prototype["onListenStart"] = StatusWidget.prototype.onListenStart;
+  StatusWidget.prototype["onListenEnd"] = StatusWidget.prototype.onListenEnd;
+  StatusWidget.prototype["getDomNode"] = StatusWidget.prototype.getDomNode;
+
+  
+  
+  VisualUpdate.prototype["getCellValue"] = VisualUpdate.prototype.getCellValue;
+  VisualUpdate.prototype["setCellValue"] = VisualUpdate.prototype.setCellValue;
+  VisualUpdate.prototype["getChangedFieldValue"] = VisualUpdate.prototype.getChangedFieldValue;
+  VisualUpdate.prototype["setHotTime"] = VisualUpdate.prototype.setHotTime;
+  VisualUpdate.prototype["setColdToHotTime"] = VisualUpdate.prototype.setColdToHotTime;
+  VisualUpdate.prototype["setHotToColdTime"] = VisualUpdate.prototype.setHotToColdTime;
+  VisualUpdate.prototype["setAttribute"] = VisualUpdate.prototype.setAttribute;
+  VisualUpdate.prototype["setStyle"] = VisualUpdate.prototype.setStyle;
+  VisualUpdate.prototype["setCellAttribute"] = VisualUpdate.prototype.setCellAttribute;
+  VisualUpdate.prototype["setCellStyle"] = VisualUpdate.prototype.setCellStyle;
+  VisualUpdate.prototype["forEachChangedField"] = VisualUpdate.prototype.forEachChangedField;
