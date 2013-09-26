1ca0092c91ece0d0f1f0b76b8cb6d1c30322af81
mone <simone.fabiano@gmail.com>
2013-09-26 15:54:23 +0200

    add (more) missing externs.

--- build/ls-externs.js
+++ build/ls-externs.js
@@ -5,3 +5,12 @@ Subscription = function() {};
 Subscription.prototype.isSubscribed = function() {};
 Subscription.prototype.getKeyPosition = function() {};
 Subscription.prototype.getCommandPosition = function() {};
+
+ItemUpdate = function() {};
+ItemUpdate.prototype.forEachChangedField = function() {};
+ItemUpdate.prototype.forEachField = function() {};
+ItemUpdate.prototype.getItemName = function() {};
+ItemUpdate.prototype.getItemPos = function() {};
+ItemUpdate.prototype.getValue = function() {};
+ItemUpdate.prototype.isSnapshot = function() {};
+ItemUpdate.prototype.isValueChanged = function() {};
