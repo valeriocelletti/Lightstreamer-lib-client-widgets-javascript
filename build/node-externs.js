6988aa24c10332f8dd62d7a138c1e00091793fa0
mone <simone.fabiano@gmail.com>
2014-04-03 15:51:25 +0200

    add missing externs

--- build/node-externs.js
+++ build/node-externs.js
@@ -2,3 +2,7 @@ process = {};
 process.execPath = {};
 process.v8 = {};
 process.node = {};
+process.versions = {};
+process.versions.node = {};
+process.versions.v8 = {};
+process.nextTick = function(){};
